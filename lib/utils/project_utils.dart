ad8b51045d233b479a5327bc6f8956b4a18aa314
Mahadi <mahadi15-14752@diu.edu.bd>
2024-05-02 22:21:53 +0600

    upload

---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,31 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/gofortravel.png",
+    "assets/projects/rideon.png",
+    "assets/projects/calculatorb.png",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/flutter.png",
+    "assets/projects/logo.png",
+    "assets/projects/flutter.png",
+  ];
+
+  static const List<String> titles = [
+    "TechWave",
+    "Rideon",
+    "Hello_Dr",
+  ];
+
+  static const List<String> description = [
+    "Tech Wave is a technology blog site which is a leading source of breaking mobile tech news, and it is intended for tech novices and experts alike.",
+    "Discover flexible car rentals spanning from one month to one year on our platform, offering convenience and freedom for your extended mobility needs.",
+    "A platform where technology meets healthcare! Hello Doctor promises more convenient and easy access to online healthcare services.",
+  ];
+
+  static const List<String> links = [
+    "https://github.com/mahadihasan77",
+    "https://github.com/mahadihasan77",
+    "https://github.com/mahadihasan77",
+  ];
+}
